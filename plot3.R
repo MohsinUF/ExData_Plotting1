2a7123427523a7728cd240d22265c76e7bf814f1
MohsinUF <67219361+MohsinUF@users.noreply.github.com>
2020-06-22 22:18:58 +0600

    Plot 3

---
+++ plot3.R
@@ -0,0 +1,20 @@
+library(tidyverse)
+
+# read data
+powerDT <- data.table::fread(input = "household_power_consumption.txt"
+                             , na.strings="?")
+
+
+
+powerDT[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
+
+
+powerDT[, dateTime := as.POSIXct(paste(Date, Time), format = "%d/%m/%Y %H:%M:%S")]
+
+
+powerDT <- powerDT[(dateTime >= "2007-02-01") & (dateTime < "2007-02-03")]
+
+png("plot3.png", width=480, height=480)
+
+
+plot(powerDT[, dateTime], powerDT[, Sub_metering_1], type="l", xlab="", ylab="Energy sub metering")
