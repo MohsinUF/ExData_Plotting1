612050b1576233b64077a25bc85bd789909d6a72
MohsinUF <67219361+MohsinUF@users.noreply.github.com>
2020-06-22 22:15:57 +0600

    Plot

--- plot1.R
+++ plot1.R
@@ -1,4 +1,4 @@
-library("data.table")
+library(tidyverse)
 
 #Reads data
 powerDT <- data.table::fread(input = "household_power_consumption.txt"
@@ -13,10 +13,10 @@ powerDT[, Date := lapply(.SD, as.Date, "%d/%m/%Y"), .SDcols = c("Date")]
 
 powerDT <- powerDT[(Date >= "2007-02-01") & (Date <= "2007-02-02")]
 
-png("plot1.png", width=480, height=480)
+png("plopng("plot1.png", width=480, height=480)
 
 
 hist(powerDT[, Global_active_power], main="Global Active Power", 
      xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
 
-dev.off()
+
